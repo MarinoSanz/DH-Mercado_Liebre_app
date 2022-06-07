b276fc160d0f3d87deb07f7aa0fa396ed83f2c70
Marino Angel <Marinoosanz@gmail.com>
2022-06-06 23:17:48 -0300

    agregado routas en app.js

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static("public"))
+const publicPath = path.resolve(__dirname, "./Public");
+app.use(express.static(publicPath));
+
+// app.use(express.static("public"))
 
 app.get('/home', (req, res) => {
     res.sendFile(__dirname + "/views/home.html")
 })
 
-app.listen(3000, () => {
-    console.log('http://localhost:3000')
-})
+app.listen(process.env.PORT || 3000, () => {
+    console.log("servidor corriendo en puerto 3000")
+});
+
+app.get("/", (req, res) => {
+    // esta forma es del Chino 
+    res.sendFile(__dirname + "/Views/home.html")
+});
+
+app.get("/login", (req, res) => {
+    //  esta forma está en el material
+    res.sendFile(path.resolve(__dirname, "./Views/login.html"))
+});
+
+
